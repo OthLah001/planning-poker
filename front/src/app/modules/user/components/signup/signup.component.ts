b50e3f9ebdc4d962c1b44f6058e17ad9e2ee0444
OthLah001 <oth.lahrimi1996@gmail.com>
2021-05-09 12:27:27 +0000

    Create Form && Manage the validity logic

--- front/src/app/modules/user/components/signup/signup.component.ts
+++ front/src/app/modules/user/components/signup/signup.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { passwordsValidator } from 'src/app/modules/user/utils/signup.functions';
+import { combineLatest, Subscription } from 'rxjs';
 
 @Component({
   selector: 'signup',
-  templateUrl: './signup.component.html'
+  templateUrl: './signup.component.html',
+  styles: [`
+    .input-icon {
+      cursor: pointer;
+    }
+  `]
 })
-export class SignupComponent implements OnInit{
+export class SignupComponent implements OnInit, OnDestroy {
   public form: FormGroup;
   public dataLoaded: boolean = false;
+
+  public hidePassword: boolean = true;
+
+  private subs: Subscription = new Subscription();
 
   constructor(
     private formBuilder: FormBuilder
@@ -18,12 +27,46 @@ export class SignupComponent implements OnInit{
     // TO-DO: check if already logged in (cookies)
 
     this.form = this.formBuilder.group({
-      email: [null, Validators.required],
+      email: [null, {
+        validators: [Validators.required, Validators.pattern('[a-zA-Z0-9\.\-_]+@[a-z]+\.[a-z]{2,}')],
+        updateOn: 'change'
+      }],
       displayName: [null, Validators.required],
-      password1: [null, Validators.required],
-      password2: [null, [Validators.required, passwordsValidator(this.form)]]
+      password1: [null, [Validators.required, Validators.minLength(8)]],
+      password2: [null, Validators.required]
     });
 
+    this.subs.add(
+      combineLatest([
+        this.form.get('password1').valueChanges,
+        this.form.get('password2').valueChanges
+      ]).subscribe(([password1, password2]) => {
+        let errors = this.form.get('password2').errors;
+  
+        if (password1 !== password2)  errors = Object.assign(errors ?? {}, { invalidMatch: true })
+        else  delete errors?.invalidMatch
+  
+        this.form.get('password2').setErrors(errors && Object.keys(errors).length > 0 ? errors : null);
+      })
+    );
+
     this.dataLoaded = true;
+  }
+
+  showHidePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  submit() {
+    if(this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
