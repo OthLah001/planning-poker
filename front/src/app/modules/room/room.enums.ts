3695636090c9443d27051ebf014e4812033994ee
OthLah001 <oth.lahrimi1996@gmail.com>
2021-06-05 10:14:14 +0000

    make the VotingSystems variable as an array

--- front/src/app/modules/room/room.enums.ts
+++ front/src/app/modules/room/room.enums.ts
@@ -1,23 +1,23 @@
 
-export const VotingSystems = {
-  Fibonacci: {
+export const VotingSystems = [
+  {
     id: '9eb65cb8-76f2-4738-b5bf-ebe6187762fe',
     name: 'Fibonacci',
     points: ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '?']
   },
-  ModifiedFibonacci: {
+  {
     id: '08d5b6f0-9857-428e-a54b-fa63854b9631',
     name: 'Modified Fibonacci',
     points: ['0', '1/2', '1', '2', '3', '5', '8', '13', '20', '40', '100', '?']
   },
-  Tshirts: {
+  {
     id: '72a84f78-e6ce-47a9-bc31-532e344b0e8d',
     name: 'T-shirts',
     points: ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', '?']
   },
-  PowerOfTwo: {
+  {
     id: '8b14e6cf-6399-4397-b792-68f917ccd6ab',
     name: 'Power of 2',
     points: ['0', '1', '2', '4', '8', '16', '32', '64', '?']
   }
-}
+]
